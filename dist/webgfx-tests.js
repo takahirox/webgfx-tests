7e6053092e822c9a305352f45f2d212786f6fdb9
Takahiro <hogehoge@gachapin.jp>
2020-11-05 13:34:32 -0800

    Update dist

--- dist/webgfx-tests.js
+++ dist/webgfx-tests.js
@@ -2719,6 +2719,59 @@
 	  nearestNeighbor(srcImageData, newImageData);
 	}
 
+	const RealRelativeOrientationSensor = typeof RelativeOrientationSensor !== 'undefined'
+	  ? RelativeOrientationSensor : undefined;
+
+	// @TODO: Support record and replay.
+
+	class MockRelativeOrientationSensor {
+	  constructor(options = {}) {
+	    this._options = options;
+	    this.quaternion = [0, 0, 0, 1];
+	    this._matrix = [
+	      1, 0, 0, 0,
+	      0, 1, 0, 0,
+	      0, 0, 1, 0,
+	      0, 0, 0, 1
+	    ];
+	  }
+
+	  populateMatrix(targetMatrix) {
+	    for (let i = 0; i < 16; i++) {
+	      targetMatrix[i] = this._matrix[i];
+	    }
+	  }
+
+	  start() {
+	  }
+
+	  // Ideally this class shoukd have all mock EventTarget methods and properties
+	  // but declaring used well ones so far.
+	  addEventListener(type, callback) {
+	  }
+
+	  removeEventListener(type, callback) {
+	  }
+	}
+
+	var FakeSensors = {
+	  enabled: false,
+	  enable: function () {
+	    if (this.enabled || !RealRelativeOrientationSensor) {
+	      return;
+	    }
+	    RelativeOrientationSensor = MockRelativeOrientationSensor;
+	    this.enabled = true;
+	  },
+	  disable: function () {
+	    if (!this.enabled || !RealRelativeOrientationSensor) {
+	      return;
+	    }
+	    RelativeOrientationSensor = RealRelativeOrientationSensor;
+	    this.enabled = false;
+	  }
+	};
+
 	var pixelmatch_1 = pixelmatch;
 
 	function pixelmatch(img1, img2, output, width, height, options) {
@@ -3943,6 +3996,10 @@
 
 	    if (!GFXTESTS_CONFIG.dontOverrideWebAudio) {
 	      WebAudioHook.enable(typeof parameters['fake-webaudio'] !== 'undefined');
+	    }
+
+	    if (!GFXTESTS_CONFIG.dontOverrideSensors) {
+	      FakeSensors.enable();
 	    }
 
 	    // @todo Use config
